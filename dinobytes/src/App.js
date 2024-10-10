805ca93734cd9c8903c30db4aa628762478e8375
72Rings <cooperfruth@gmail.com>
2024-10-10 16:02:07 -0500

    please work

--- dinobytes/src/App.js
+++ dinobytes/src/App.js
@@ -1,15 +1,34 @@
-// App.js
 import React from 'react';
+import { Button, Card, Navbar, Container, Nav } from 'react-bootstrap';
 import logo from './logo.svg';
-import { Button } from 'react-bootstrap';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">My React App</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Button variant="primary">React-Bootstrap Button</Button>
+        <Button variant="primary">Bootstrap Button</Button>
+        <Card style={{ width: '18rem', marginTop: '20px' }}>
+          <Card.Img variant="top" src={logo} />
+          <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Card.Text>
+              This is a simple card example using Bootstrap in a React app.
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
--- dinobytes/src/index.js
+++ dinobytes/src/index.js
@@ -1,48 +1,20 @@
 import React from 'react';
-import { Button, Card, Navbar, Container, Nav } from 'react-bootstrap';
-import logo from './logo.svg';
-import './App.css';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar bg="dark" variant="dark">
-        <Container>
-          <Navbar.Brand href="#home">My React App</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Button variant="primary">Bootstrap Button</Button>
-        <Card style={{ width: '18rem', marginTop: '20px' }}>
-          <Card.Img variant="top" src={logo} />
-          <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            <Card.Text>
-              This is a simple card example using Bootstrap in a React app.
-            </Card.Text>
-            <Button variant="primary">Go somewhere</Button>
-          </Card.Body>
-        </Card>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+//hello dudes
+//YOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
