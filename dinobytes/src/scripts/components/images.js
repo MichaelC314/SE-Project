d220eaa8307bc2c1d83e8c3dfb10a04472637be8
rcana101 <rachel.elwc@gmail.com>
2024-12-02 22:37:56 -0600

    images is now in a map

--- dinobytes/src/scripts/components/images.js
+++ dinobytes/src/scripts/components/images.js
@@ -1,24 +1,23 @@
 import React, { useState } from 'react';
 
-const images = [
-'dinobytes\src\img\ProfilePictures\FlyDino1.png',
-'dinobytes\src\img\ProfilePictures\FlyDino2.png',
-'dinobytes\src\img\ProfilePictures\FlyDino3.png',
-'dinobytes\src\img\ProfilePictures\FlyDino4.png',
-'dinobytes\src\img\ProfilePictures\FlyDino5.png',
-'dinobytes\src\img\ProfilePictures\MoonKightDino.png',
-'dinobytes\src\img\ProfilePictures\MrMoonDino.png',
-'dinobytes\src\img\ProfilePictures\ScaleDino1.png',
-'dinobytes\src\img\ProfilePictures\ScaleDino2.png',
-'dinobytes\src\img\ProfilePictures\ScaleDino3.png',
-'dinobytes\src\img\ProfilePictures\ScaleDino4.png',
-'dinobytes\src\img\ProfilePictures\ScaleDino5.png',
-'dinobytes\src\img\ProfilePictures\TRexDino1.png',
-'dinobytes\src\img\ProfilePictures\TRexDino2.png',
-'dinobytes\src\img\ProfilePictures\TRexDino3.png',
-'dinobytes\src\img\ProfilePictures\TRexDino4.png',
-'dinobytes\src\img\ProfilePictures\TRexDino5.png',
-  ];
-  
-  export default images;
-  
+const imagesMap = new Map([
+  ["FlyDino1", "dinobytes/src/img/ProfilePictures/FlyDino1.png"],
+  ["FlyDino2", "dinobytes/src/img/ProfilePictures/FlyDino2.png"],
+  ["FlyDino3", "dinobytes/src/img/ProfilePictures/FlyDino3.png"],
+  ["FlyDino4", "dinobytes/src/img/ProfilePictures/FlyDino4.png"],
+  ["FlyDino5", "dinobytes/src/img/ProfilePictures/FlyDino5.png"],
+  ["MoonKnightDino", "dinobytes/src/img/ProfilePictures/MoonKightDino.png"],
+  ["MrMoonDino", "dinobytes/src/img/ProfilePictures/MrMoonDino.png"],
+  ["ScaleDino1", "dinobytes/src/img/ProfilePictures/ScaleDino1.png"],
+  ["ScaleDino2", "dinobytes/src/img/ProfilePictures/ScaleDino2.png"],
+  ["ScaleDino3", "dinobytes/src/img/ProfilePictures/ScaleDino3.png"],
+  ["ScaleDino4", "dinobytes/src/img/ProfilePictures/ScaleDino4.png"],
+  ["ScaleDino5", "dinobytes/src/img/ProfilePictures/ScaleDino5.png"],
+  ["TRexDino1", "dinobytes/src/img/ProfilePictures/TRexDino1.png"],
+  ["TRexDino2", "dinobytes/src/img/ProfilePictures/TRexDino2.png"],
+  ["TRexDino3", "dinobytes/src/img/ProfilePictures/TRexDino3.png"],
+  ["TRexDino4", "dinobytes/src/img/ProfilePictures/TRexDino4.png"],
+  ["TRexDino5", "dinobytes/src/img/ProfilePictures/TRexDino5.png"],
+]);
+
+export default imagesMap;
